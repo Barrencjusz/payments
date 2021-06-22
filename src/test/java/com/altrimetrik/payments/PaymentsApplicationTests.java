334bc7eff30cb3d2db496eebc157fad6dbe8c226
Piotr Larysz <piotr.larysz@olx.pl>
2021-06-22 14:12:32 +0200

    Initial commit

---
+++ src/test/java/com/altrimetrik/payments/PaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altrimetrik.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
